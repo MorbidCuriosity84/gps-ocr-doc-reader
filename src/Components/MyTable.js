368a72b3f1efc4db284ef077f6d4d8a9f4bda2f4
Mark Nelson <40428606@live.napier.ac.uk>
2020-12-23 15:38:00 +0000

    Added table and connected to server

--- src/Components/MyTable.js
+++ src/Components/MyTable.js
@@ -3,6 +3,42 @@ import Table from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class MyTable extends Component {
+
+    constructor(props) {
+        super (props);
+
+        this.state = {
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        this.getDataFromAPI()
+            .then(res => this.setState({ data: res.data }))
+            .catch(err => console.log(err));
+    }
+
+    async getDataFromAPI() {
+        const response = await fetch ('http://localhost:5000/API/data');
+        const body = await response.json ();
+        if (response.status !== 200) {
+            throw Error (body.message)
+        }
+        return body;
+    }
+
+    renderData(data, index) {
+        return (
+            <tr key={index}>
+                <td>{data.ID}</td>
+                <td>{data.PartName}</td>
+                <td>{data.Date}</td>
+                <td>{data.Status}</td>
+                <td>{data.Link}</td>
+            </tr>
+        )
+    }
+
     render() {
         return (
             <div>
@@ -17,20 +53,7 @@ class MyTable extends Component {
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Engine</td>
-                        <td>29th Nov 2020</td>
-                        <td>Repaired</td>
-                        <td>n/a</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Engine</td>
-                        <td>29th Nov 2020</td>
-                        <td>Repaired</td>
-                        <td>n/a</td>
-                    </tr>
+                        {this.state.data.map(this.renderData)}
                     </tbody>
                 </Table>
             </div>
